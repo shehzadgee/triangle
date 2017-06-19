2558f1fb27c441733b0dadef549d77d960bc5a42
Khurram Shahzad <kshah@softserveinc.com>
2017-06-19 20:35:24 +0200

    Some additional Tests and Readme Updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# triangle
+# Triangle
 This is one sample implementation of identifying triangle type based on length of sides.
+
+Solution is self explanatory and tests explain input and expected behavior of the program. Program takes 3 double fields as arguments and output the type of triangle or display's appropriate exception.
--- src/test/java/com/coding/shape/triangle/TriangleFactoryTest.java
+++ src/test/java/com/coding/shape/triangle/TriangleFactoryTest.java
@@ -3,6 +3,7 @@
  */
 package com.coding.shape.triangle;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -21,10 +22,30 @@ public class TriangleFactoryTest {
 		triangleFactory.createTriangle(1D, 2D, 3D);
 	}
 	
+	@Test
 	public void testCreateTriangleWithEqualSides() throws TriangleException{
 		Triangle triangle = triangleFactory.createTriangle(2D, 2D, 2D);
-		
+		Assert.assertEquals(TriangleType.EQUILATERAL, triangle.getType());
 	}
+	
+	@Test
+	public void testCreateTriangleWithTwoEqualSidesOnly() throws TriangleException{
+		Triangle triangle = triangleFactory.createTriangle(2D, 2D, 3D);
+		Assert.assertEquals(TriangleType.ISOSCELES, triangle.getType());
+	}
+	
+	@Test
+	public void testCreateTriangleWithDifferentSides() throws TriangleException{
+		Triangle triangle = triangleFactory.createTriangle(2D, 3D, 4D);
+		Assert.assertEquals(TriangleType.SCALENE, triangle.getType());
+	}
+	
+	@Test(expected=TriangleException.class)
+	public void testCreateTriangleWithInvalidLength() throws TriangleException{
+		Triangle triangle = triangleFactory.createTriangle(0, 3D, 4D);
+	}
+	
+	
 	
 	
 	
